94cb2b2815486cec1dac47844006d4b42ea139f2
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2020-10-30 12:43:19 +0100

    python codespace

--- workflows/PythonCodespace/hello.py
+++ workflows/PythonCodespace/hello.py
@@ -1,2 +1,7 @@
+// https://techcommunity.microsoft.com/t5/educator-developer-blog/using-visual-studio-code-from-a-docker-image-locally-or-remotely/ba-p/1264988
+import numpy as np
+
+x = np.arange(8000)
+
 print ("Hello")
 
